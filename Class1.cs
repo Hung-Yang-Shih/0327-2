ec337ba8b604c0ff8483e7b355f4baff95358a52
Hung-Yang-Shih <d11416204@gs.takming.edu.tw>
2026-03-27 10:52:04 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Manager
+{
+	public readonly DateTime Startup;
+
+	public Manager()
+	{
+        Startup = DateTime.Now;
+    }
+
+	public DateTime GetStarup()
+	{
+		return Startup;
+	}
+	
+		}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _0327_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Manager m = new Manager();
+
+            Console.WriteLine(m.GetStarup().ToString());
+
+            //m.Startup = DateTime.UtcNow;
+
+            Console.WriteLine(m.GetStarup().ToString());
+        }
+    }
+}
